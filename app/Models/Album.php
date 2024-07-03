5fb98fb1a481f3f8558e172da7f77854b395d5e0
dceleste35 <danceleste974@gmail.com>
2024-07-03 16:48:34 +0200

    feat: Add Album and Artist models and migrations

---
+++ app/Models/Album.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Album extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'release_date',
+        'artist_id',
+    ];
+
+    protected $casts = [
+        'release_date' => 'datetime',
+    ];
+
+    public function artist(): BelongsTo
+    {
+        return $this->belongsTo(Artist::class);
+    }
+}
