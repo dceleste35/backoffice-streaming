f3e0148ecec0b1e883fec5713ee2cbaed5ae42d2
dceleste35 <danceleste974@gmail.com>
2024-07-03 18:42:24 +0200

    feat: Add SubscriptionEnum for managing subscription types

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers\SubscriptionsRelationManager;
 use App\Models\User;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
@@ -74,7 +75,7 @@ class UserResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            SubscriptionsRelationManager::class,
         ];
     }
 
--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\SubscriptionEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -12,6 +13,10 @@ class Subscription extends Model
 
     protected $fillable = [
         'name',
+    ];
+
+    protected $casts = [
+        'name' => SubscriptionEnum::class,
     ];
 
     public function users(): BelongsToMany
