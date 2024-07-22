7c0946a4aae16c552152c12c8f0f80547055355b
dceleste35 <danceleste974@gmail.com>
2024-07-22 15:27:06 +0200

    feat: Add GenreResource for managing genre resources
    This commit adds the `GenreResource` class for managing genre resources in the Filament admin panel. It includes form and table configurations, as well as pages for listing, creating, and editing genre records. The resource provides functionality for adding a name and description for genres.

---
+++ app/Filament/Resources/GenreResource.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\GenreResource\Pages;
+use App\Models\Genre;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class GenreResource extends Resource
+{
+    protected static ?string $model = Genre::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
+
+    protected static ?string $navigationLabel = 'Genres';
+
+    protected static ?string $pluralModelLabel = 'Genres';
+
+    protected static ?string $modelLabel = 'Genre';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('Nom')
+                    ->columnSpanFull()
+                    ->required(),
+                RichEditor::make('description')
+                    ->columnSpanFull()
+                    ->columnStart(1),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('music_count')->counts('music')
+                    ->label('Nombre de musiques'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListGenres::route('/'),
+            'create' => Pages\CreateGenre::route('/create'),
+            'edit' => Pages\EditGenre::route('/{record}/edit'),
+        ];
+    }
+}
