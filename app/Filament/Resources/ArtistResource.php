1a4023793243cf87f1e9c8b0f714cc597ad518dc
dceleste35 <danceleste974@gmail.com>
2024-07-22 14:52:45 +0200

    feat: Add ArtistResource for managing artist resources
    This commit adds the `ArtistResource` class for managing artist resources in the Filament admin panel. It includes form and table configurations, as well as pages for listing, creating, and editing artist records. The resource provides functionality for adding a name, music count, and albums count for artists.

---
+++ app/Filament/Resources/ArtistResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ArtistResource\Pages;
+use App\Models\Artist;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class ArtistResource extends Resource
+{
+    protected static ?string $model = Artist::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-plus';
+
+    protected static ?string $navigationLabel = 'Artistes';
+
+    protected static ?string $pluralModelLabel = 'Artistes';
+
+    protected static ?string $modelLabel = 'Artiste';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable()
+                    ->label('Nom'),
+                TextColumn::make('music_count')->counts('music')
+                    ->label('Nombre de musiques'),
+                TextColumn::make('albums_count')->counts('albums')
+                    ->label('Nombre d\'albums'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                //
+            ])
+            ->bulkActions([
+                //
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListArtists::route('/'),
+            'create' => Pages\CreateArtist::route('/create'),
+            'edit' => Pages\EditArtist::route('/{record}/edit'),
+        ];
+    }
+}
